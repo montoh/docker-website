edf8b50061c5ae2b7bd39350ac75e970f3022e49
MonToh <oma.egboh@gmail.com>
2020-06-30 04:27:06 +0100

    added more comment

--- setup.sh
+++ setup.sh
@@ -4,13 +4,14 @@ echo "================"
 echo "Builing Image... "
 echo "================"
 
-
+# build the docker image
 docker build -t bash-setup:1.0 -t bash-setup:latest .
 
 echo "================"
 echo "Running Image... "
 echo "================"
 
+# run the docker image
 docker run -d --name bash-container -p 8282:80 bash-setup:latest
 
 echo "================"
